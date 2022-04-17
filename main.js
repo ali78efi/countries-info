eef1f26a28a801a6077fbb2893a132eb0d05cecd
Ali Eftekhari <ali.efi.1376@gmail.com>
2022-04-17 21:51:59 +0430

    scirpt completed

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
 //https://restcountries.com/v3.1/name/${country}
+const input = document.getElementById('input');
+const form = document.querySelector('form');
+const container = document.querySelector('.container');
+const msg = document.querySelector('.msg');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addCountry(input.value);
+    input.value = ''
+})
+
+
+
+
+function addCountry(country) {
+    const url = `https://restcountries.com/v3.1/name/${country}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const info = data[0];
+            const {
+                name,
+                flags,
+                population,
+                capital
+            } = info;
+            const markup = `
+            <div class="flag">
+                <img src="${flags.svg}" alt="${name.common}'s flag">
+                </div>
+            <div class="description">
+            <h3>${name.common}</h3>
+            <h4>capital: ${capital[0]}</h4>
+            <h4>Population: ${(+population / 1000000).toFixed(1)}M</h4>
+            </div>
+            `;
+            const countryDiv = document.createElement('div');
+            countryDiv.classList.add('country');
+            countryDiv.innerHTML = markup;
+            container.appendChild(countryDiv);
+        })
+        .catch(err => {
+            alert("country not found");
+            console.log(err);
+        })
+}
